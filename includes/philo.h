19c3b2f9185625b019534d780b9889ec44d72ea0
Alice NIEZGODZKI <aniezgod@e2r7p3.clusters.42paris.fr>
2023-03-17 11:02:51 +0100

    maj

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: aniezgod <aniezgod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 16:54:58 by aniezgod          #+#    #+#             */
-/*   Updated: 2023/03/14 14:48:30 by aniezgod         ###   ########.fr       */
+/*   Updated: 2023/03/16 15:25:04 by aniezgod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 typedef struct s_philo {
     int nb_times_eat;
     int pos;
+    int rfork;
+    int lfork;
+    pthread_t thread_philo;
 }               t_philo;
 
 typedef struct s_data {
     int nb_philo;
     struct s_philo *philo;
+    phtread_mutex_t *forks
 }               t_data;
 
 void    ft_init(t_data *data);
--- srcs/init.c
+++ srcs/init.c
@@ -6,11 +6,16 @@
 /*   By: aniezgod <aniezgod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 14:29:24 by aniezgod          #+#    #+#             */
-/*   Updated: 2023/03/14 14:54:53 by aniezgod         ###   ########.fr       */
+/*   Updated: 2023/03/16 15:31:56 by aniezgod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void    start_lunch(void *philo)
+{
+    return ;
+}
 
 void    ft_init(t_data *data)
 {
@@ -21,6 +26,36 @@ void    ft_init(t_data *data)
     {
         data->philo[i].pos = i + 1;
         data->philo[i].nb_times_eat = 0;
+        i++;
+        data->philo[i].lfork = i;
+        if (i == data->nb_philo)
+            data->philo[i].rfork = 1;
+        else
+            data->philo[i].rfork = i + 1;
+    }
+}
+
+int create_thread(t_data *data)
+{
+    int i;
+
+    i = 1;
+    while(i <= data->nb_philo)
+    {
+        pthread_create(&data->philo->thread_philo, NULL, start_lunch, &data->philo[i]);
+        i++;
+    }
+    return (1);
+}
+
+int init_mutex(t_data *data)
+{
+    int i;
+
+    i = 1;
+    while(i <= data->nb_philo)
+    {
+        pthread_mutex_init(forks[i], NULL);
         i++;
     }
 }
